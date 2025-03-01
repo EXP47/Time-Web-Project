ffb910d0f70dd4dc63f85b182926296d63158bfd
MXEXP <exploder47z@gmail.com>
2025-03-01 12:55:34 -0500

    Added TextField

--- time-web/src/App.tsx
+++ time-web/src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
 import TestButton from './TestButton';
 
 const theme = createTheme();
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Normalizes styles */}
       <div className="App">
         <div className="upper-content">
-          <div className="upper-left-text">Time Web</div>
+          <div className="upper-top-text">Time Web</div>
           <img src="/images/globe_anim.gif" alt="Logo" />
         </div>
-        <div className="button1_signin">
-          <button>Sign In</button>
+        <div className="input-button-container">
+          <TextField
+            label="type here"
+            variant="outlined"
+            value={inputValue}
+            onChange={handleInputChange}
+            className="custom-text-field"
+          />
+          <button className="custom-button">Link MAL</button>
         </div>
       </div>
     </ThemeProvider>
